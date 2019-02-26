9c2fc714b174fbbd24edaab492b0cb9a3048d544
Vinícius Morais Silva <vmsabc@gmail.com>
2019-02-26 20:34:38 -0300

    Fixed menu selection
    Insert HashRouter

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,9 +3,12 @@ import Blog from './Blog.js';
 import Livros from './Livros.js';
 import Autor from './Autor.js';
 import Galeria from './Galeria.js';
-import TopMenu from '../components/TopMenu.js'
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
 
 export default class MainPage extends Component {
 
@@ -21,9 +24,24 @@ export default class MainPage extends Component {
   render() {
 
     return (
-      <div className="MainPage">
-        <TopMenu/>
-      </div>
+      <HashRouter>
+        <div>
+          <div className="TopHead">
+            <ul className="header">
+             <li className="TopLinks"><NavLink exact to="/">Blog</NavLink></li>
+              <li className="TopLinks"><NavLink to="/autor">Autor</NavLink></li>
+              <li className="TopLinks"><NavLink to="/galeria">Galeria</NavLink></li>
+              <li className="TopLinks"><NavLink to="/Livros">Livros</NavLink></li>
+            </ul>
+          </div>
+          <div className="content">
+            <Route exact path="/" component={Blog}/>
+            <Route path="/autor" component={Autor}/>
+            <Route path="/galeria" component={Galeria}/>
+            <Route path="/livros" component={Livros}/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
